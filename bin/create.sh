8001cf11225b5483bb0eb83e06f2f00af33331de
sandy <sandy@mac.com>
2020-01-10 00:25:17 +1100

    cleanup

--- bin/create.sh
+++ bin/create.sh
@@ -2,21 +2,54 @@
 
 ROOT=`dirname "$0"`
 ROOT=`cd "$ROOT"/.. ; pwd`
+PROJECT_NAME=$1
 
-PROJECT=$1
-if [ ! -d "${PROJECT}" ]
+if [[ "$PROJECT_NAME" == *\/* ]] || [[ "$string" == *\\* ]]
 then
-	echo "${PROJECT} is not a folder"
+	echo "project name cannot be a path."
 	exit -1
 fi
 
-PROJECT=`cd "${PROJECT}" ; pwd`
+. ${ROOT}/.env
+
+PROJECT="${BUILDER_SHARED_FOLDER}/${PROJECT_NAME}"
+if [ ! -d "${PROJECT}" ]
+then
+	echo "${PROJECT} does not exist. Creating default project."
+	mkdir "${PROJECT}"
+
+cat << EOF > "${PROJECT}/main.cpp"
+#include <iostream>
+
+int main( int argc, char **argv )
+{
+}
+EOF
+
+cat << 'EOF' > "${PROJECT}/CMakeLists.txt"
+cmake_minimum_required(VERSION 3.16)
+project(PROJECT_NAME)
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+enable_testing()
+
+add_executable(PROJECT_NAME)
+target_sources(PROJECT_NAME PRIVATE
+	main.cpp
+)
+EOF
+
+perl -pi -e "s|PROJECT_NAME|$PROJECT_NAME|" "${PROJECT}/CMakeLists.txt"
+
+
+fi
+
 echo "creating workspace for ${PROJECT}"
 
-WORKSPACE_NAME=`basename "${PROJECT}"`
-
-TEMPLATE="${ROOT}/Workspaces/TEMPLATE.code-workspace"
-WORKSPACE="${ROOT}/Workspaces/${WORKSPACE_NAME}.code-workspace"
+mkdir -p "${ROOT}/Workspaces"
+TEMPLATE="${ROOT}/bin/TEMPLATE.code-workspace"
+WORKSPACE="${ROOT}/Workspaces/${PROJECT_NAME}.code-workspace"
 
 if [ -f "${WORKSPACE}" ]
 then
