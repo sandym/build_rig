eb2c14fe83e719fc15ced28ba4b3237b3030ccc5
sandy <sandy@mac.com>
2020-12-21 23:22:09 +1100

    unify darwin.sh and driver.sh

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# container: container name
+# platform: darwin or container name
 #
 # triplet:  action-toolset-type
 #
@@ -10,7 +10,7 @@
 #		type:    debug, release, asan, tsan, ubsan
 #
 
-CONTAINER=$1
+PLATFORM=$1
 TRIPLET=$2
 PROJECT=$3
 PROJECT_NAME=$(basename "${PROJECT}")
@@ -18,14 +18,14 @@ PROJECT_NAME=$(basename "${PROJECT}")
 usage()
 {
 	echo "usage:"
-	echo "  ./docker.sh container triplet project"
+	echo "  ./driver.sh platform triplet project_path"
 	echo ""
-	echo "     container: name of a container to run the build"
-	echo "     triplet:   action-type-toolset"
+	echo "     platform: darwin or name of a container to run the build"
+	echo "     triplet:   action-toolset-type"
 	echo "                where:"
 	echo "                   action:  build, test, clean"
-	echo "                   type:    debug, release, asan, tsan"
 	echo "                   toolset: gcc8, gcc9, clang"
+	echo "                   type:    debug, release, asan, tsan"
 	echo "     project:   path to project to build, should have a"
 	echo "                CMakeLists.txt"
 	echo ""
@@ -124,75 +124,8 @@ do_cmake()
 	fi
 }
 
-if [ ! -f "/.dockerenv" ]
-then
-	# in host
-	SCRIPTS=$(dirname "$0")
-	SCRIPTS=$(cd "${SCRIPTS}" ; pwd)
-	cd "${SCRIPTS}"
-
-	source "../.env"
-
-	# build syndir of needed
-	if [ ! -f "syncdir_host" ] || [ "syncdir.go" -nt "syncdir_host" ]
-	then
-		echo "building syncdir..."
-	 	$(go build -o syncdir_host)
-	 	$(GOOS=linux GOARCH=amd64 go build -o syncdir_linux)
-	fi
-
-	"./syncdir_host" -scan "${PROJECT}"
-
-	cd "${SCRIPTS}/.."
-
-	export MSYS_NO_PATHCONV=1
-	docker-compose run --rm ${CONTAINER} \
-			/scripts/docker.sh ${CONTAINER} ${TRIPLET} ${PROJECT}
-
-	echo ""
-	echo "done: ${CONTAINER} ${TRIPLET}"
-
-else
-	# in container, for a linux build
-
-	# adjust TOOLSET
-	case ${CONTAINER} in
-		centos7_builder)
-			centos7_toolset
-			;;
-		alpine_builder)
-			alpine_toolset
-			;;
-		ubuntu_builder)
-			ubuntu_toolset
-			;;
-		*)
-			;;
-	esac
-
-	BIN_DIR=/work/${PROJECT_NAME}/${CONTAINER%_builder}/${TOOLSET}-${TYPE}
-
-	if [ "${ACTION}" = "clean" ]
-	then
-		if [ -d "${BIN_DIR}" ]
-		then
-			cd "${BIN_DIR}"
-
-			/script/syncdir_linux -clean "/work/${PROJECT_NAME}/src"
-			if [ -f build.ninja ]
-			then
-				ninja clean
-			else
-				rm -rf * .ninja*
-			fi
-		fi
-
-		exit 0
-	fi
-
-	/scripts/syncdir_linux -sync "/share/${PROJECT_NAME}" "/work/${PROJECT_NAME}/src"
-	echo ""
-
+run_build()
+{
 	echo "PATH = ${PATH}"
 	echo "building in ${BIN_DIR}"
 	echo ""
@@ -223,4 +156,128 @@ else
 			usage
 			;;
 	esac
+}
+
+if [ "${PLATFORM}" = "darwin" ]
+then
+	BUILD_DIR=~/darwin_build/"${PROJECT_NAME}"
+	case "${TOOLSET}" in
+		xcode)
+			case "${ACTION}" in
+				build)
+					mkdir -p "${BUILD_DIR}/xcode"
+					cd "${BUILD_DIR}/xcode"
+					cmake -G Xcode ${PROJECT_PATH}
+					open *.xcodeproj
+					exit 0
+					;;
+				clean)
+					rm -rf "${BUILD_DIR}/xcode"
+					exit 0
+					;;
+				*)
+					echo "unsupported action for xcode: ${ACTION}"
+					exit -1
+					;;
+			esac
+			;;
+		clang)
+			BIN_DIR=${BUILD_DIR}/${TYPE}
+			if [ "${ACTION}" = "clean" ]
+			then
+				if [ -d "${BIN_DIR}" ]
+				then
+					cd "${BIN_DIR}"
+					if [ -f build.ninja ]
+					then
+						ninja clean
+					else
+						rm -rf * .ninja*
+					fi
+				fi
+				exit 0
+			fi
+
+			run_build
+			;;
+		*)
+			echo "unsupported toolset for darwin: ${TOOLSET}"
+			exit -1
+			;;
+	esac
+else
+
+if [ ! -f "/.dockerenv" ]
+then
+	# in host
+	SCRIPTS=$(dirname "$0")
+	SCRIPTS=$(cd "${SCRIPTS}" ; pwd)
+	cd "${SCRIPTS}"
+
+	source "../.env"
+
+	# build syndir of needed
+	if [ ! -f "syncdir_host" ] || [ "syncdir.go" -nt "syncdir_host" ]
+	then
+		echo "building syncdir..."
+	 	$(go build -o syncdir_host)
+	 	$(GOOS=linux GOARCH=amd64 go build -o syncdir_linux)
+	fi
+
+	"./syncdir_host" -scan "${PROJECT}"
+
+	cd "${SCRIPTS}/.."
+
+	export MSYS_NO_PATHCONV=1
+	docker-compose run --rm ${PLATFORM} \
+			/scripts/docker.sh ${PLATFORM} ${TRIPLET} ${PROJECT}
+
+	echo ""
+	echo "done: ${PLATFORM} ${TRIPLET}"
+
+else
+	# in container, for a linux build
+
+	# adjust TOOLSET
+	case ${PLATFORM} in
+		centos7_builder)
+			centos7_toolset
+			;;
+		alpine_builder)
+			alpine_toolset
+			;;
+		ubuntu_builder)
+			ubuntu_toolset
+			;;
+		*)
+			;;
+	esac
+
+	BIN_DIR=/work/${PROJECT_NAME}/${PLATFORM%_builder}/${TOOLSET}-${TYPE}
+
+	if [ "${ACTION}" = "clean" ]
+	then
+		if [ -d "${BIN_DIR}" ]
+		then
+			cd "${BIN_DIR}"
+
+			/script/syncdir_linux -clean "/work/${PROJECT_NAME}/src"
+			if [ -f build.ninja ]
+			then
+				ninja clean
+			else
+				rm -rf * .ninja*
+			fi
+		fi
+
+		exit 0
+	fi
+
+	/scripts/syncdir_linux -sync "/share/${PROJECT_NAME}" "/work/${PROJECT_NAME}/src"
+	echo ""
+
+	run_build
+
+fi
+
 fi
