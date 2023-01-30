02db9daa32038e0ac2ea9b30a211272f1fcbb441
sandy <sandy@mac.com>
2023-01-30 22:24:54 +1100

    formatting

--- watcher/src/index.ts
+++ watcher/src/index.ts
@@ -6,49 +6,52 @@
 		SOURCE_DIR={} BINARY_DIR={} OPTIONS={cmake options} npm run
 */
 
-const chokidar = require('chokidar');
-const exec = require('child_process').exec;
+const chokidar = require( 'chokidar' );
+const exec = require( 'child_process' ).exec;
 
 const source_dir = process.env['SOURCE_DIR'];
 const binary_dir = process.env['BINARY_DIR'];
 const options = process.env['OPTIONS'];
 
-if (source_dir == null)
+if ( source_dir == null )
 {
-	console.log("missing SOURCE_DIR");
-	process.exit(1);
+	console.log( "missing SOURCE_DIR" );
+	process.exit( 1 );
 }
-if (binary_dir == null)
+if ( binary_dir == null )
 {
-	console.log("missing BINARY_DIR");
-	process.exit(1);
+	console.log( "missing BINARY_DIR" );
+	process.exit( 1 );
 }
 
-const watcher = chokidar.watch(`${source_dir}/**`, {
-	persistent: true
-});
+const watcher = chokidar.watch(
+	`${source_dir}/**`,
+	{ persistent: true }
+);
 
 var inProgress = false;
 
-watcher.on('all',
+watcher.on( 'all',
 	() =>
 	{
-		if (inProgress)
+		if ( inProgress )
 			return;
 		
 		inProgress = true;
-		setTimeout(() =>
+		setTimeout(
+			() =>
 			{
 				var cmd = 'cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug';
-				if (options)
+				if ( options )
 				{
 					cmd += ` ${options}`;
 				}
 				cmd += ` ${source_dir}`;
-				console.log(cmd);
-				var cmake = exec( cmd, { cwd: binary_dir }, );
-				cmake.stdout.pipe(process.stdout);
+				console.log( cmd );
+				var cmake = exec( cmd, { cwd: binary_dir } );
+				cmake.stdout.pipe( process.stdout );
 				inProgress = false;
-			}, 100 );
+			}, 100
+		);
 	}
 );
